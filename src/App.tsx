c655e646bb757d75ab26b92348a7874e5b30b0fb
wenza007 <overkung4399@gmail.com>
2025-09-08 10:04:57 +0700

    GradeApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <ButtonAdd />
-        
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.tsx'
+import GradeApp from './components/GradeApp'
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <GradeApp />
+  </StrictMode>,
+)
